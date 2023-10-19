efdeda4990dba3ea8f7db9b2c188ea1305ef8a9f
theArchitect <akhil.hem@outlook.com>
2023-10-19 06:07:38 -0400

    First Upload

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "ArchQuest",
+  description: "Get new questions everyday",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
